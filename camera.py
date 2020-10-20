7cd1a62c7aa49fdc6cff58dde3701117d876faa5
layth <54488305+glayf@users.noreply.github.com>
2020-10-20 17:28:23 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,10 +4,10 @@ import PIL
 from time import sleep
 
 avgR, avgG, avgB = 0, 0, 0 
-XL = 280
-XR = 631
-YB = 383
-YT = 560
+XL = 570
+XR = 670
+YB = 171
+YT = 271
 pixels = (XR - XL) * (YT - YB)
 
 camera = PiCamera()
@@ -19,18 +19,18 @@ camera.stop_preview()
 image = PIL.Image.open("/home/pi/aquarius/images/image.jpg")
 image_rgb = image.convert("RGB")
 
-
-for x in range(XL, XR):
-    for y in range(YB, YT):
-        rgb_pixel_value = image_rgb.getpixel((x, y))
-        avgR += rgb_pixel_value[0]
-        avgG += rgb_pixel_value[1]
-        avgB += rgb_pixel_value[2]
-R = avgR / pixels
-G = avgG / pixels
-B = avgB / pixels
-print(round(R), round(G), round(G))
-
+for no in range(10):
+	for x in range(XL, XR):
+	    for y in range(YB, YT):
+		rgb_pixel_value = image_rgb.getpixel((x, y))
+		avgR += rgb_pixel_value[0]
+		avgG += rgb_pixel_value[1]
+		avgB += rgb_pixel_value[2]
+	R = avgR / pixels
+	G = avgG / pixels
+	B = avgB / pixels
+	print(round(R), round(G), round(G))
+	sleep(1)
 '''
 measures = 5
 for x in range(measures):
