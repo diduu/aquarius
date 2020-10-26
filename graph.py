d88b65feac4d673b7f837ba425fefd43e2d21f6d
layth <54488305+glayf@users.noreply.github.com>
2020-10-26 18:22:18 +0000

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+colnames = ['DROPS', 'R', 'G', 'B']
+df = pd.read_csv('results.csv', names=colnames)
+plt.figure(figsize=(30,5))
+plt.grid(True)
+y1 = df['R']
+y2 = df['G']
+y3 = df['B']
+x = df['DROPS']
+print(df)
+y1.rolling(window=7).mean().plot()
+plt.scatter(x, y1, color='red', s=5)
+y2.rolling(window=7).mean().plot()
+plt.scatter(x, y2, color='green', s=5)
+y3.rolling(window=7).mean().plot()
+plt.scatter(x, y3, color='blue', s=5)
+plt.xticks(df.DROPS[::10])
+plt.ylabel("RGB Value")
+plt.show()
